66fd4563f937ff17dc817ef47a42d7d7dd79d9b7
tiffany <tiffanyphilippi@hotmail.com>
2019-08-26 20:14:04 -0300

    aula antes da prova

---
+++ serv-rede-web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>PAGE</title>
+</head>
+<body>
+    <?php
+        echo "<p>Olá Mundo</p>";
+    ?>
+</body>
+</html>
